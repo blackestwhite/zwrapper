faeb805147735394e41028528c13fbc55f8101d1
blackestwhite <pesaregoal@gmail.com>
2023-07-22 07:00:57 +0330

    feat: add get functionality

--- service/payment.go
+++ service/payment.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/blackestwhite/zwrapper/db"
 	"github.com/blackestwhite/zwrapper/entity"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -20,4 +21,17 @@ func (p *PaymentService) Create(instance entity.Payment) (inserted entity.Paymen
 
 	_, err = db.Client.Database("zwrapper").Collection("payments").InsertOne(ctx, instance)
 	return instance, err
+}
+
+func (p *PaymentService) Get(id string) (payment entity.Payment, err error) {
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	err = db.Client.Database("zwrapper").Collection("payments").FindOne(ctx, bson.M{"_id": objectID}).Decode(&payment)
+	return
 }
