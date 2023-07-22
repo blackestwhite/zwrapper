93ef24f0b55cb38fed2b57d73d6482de3f7c7bea
blackestwhite <pesaregoal@gmail.com>
2023-07-22 05:09:39 +0330

    add POST http client

--- utils/utils.go
+++ utils/utils.go
@@ -1,12 +1,38 @@
 package utils
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
+	"net/http"
+
+	"github.com/blackestwhite/zwrapper/entity"
 )
 
 func Hash(s string) string {
 	h := sha256.New()
 	h.Write([]byte(s))
 	return hex.EncodeToString(h.Sum(nil))
+}
+
+func Post(wp entity.WebhookPayload) (err error) {
+	b := new(bytes.Buffer)
+	err = json.NewEncoder(b).Encode(wp)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", wp.URL, b)
+	if err != nil {
+		return err
+	}
+	req.Header = http.Header{
+		"Content-Type":               {"application/json"},
+		"x-oremote-api-access-token": {wp.Key},
+	}
+
+	client := http.Client{}
+	_, err = client.Do(req)
+	return
 }
