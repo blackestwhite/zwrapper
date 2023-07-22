c2f58a0035b84e5b150b81fe2ef6046e9acc7ac3
blackestwhite <pesaregoal@gmail.com>
2023-07-22 06:53:01 +0330

    feat: get by token

--- router/middleware/payment.go
+++ router/middleware/payment.go
@@ -1,18 +1,16 @@
 package middleware
 
 import (
-	"context"
 	"net/http"
-	"time"
 
 	"github.com/blackestwhite/presenter"
-	"github.com/blackestwhite/zwrapper/db"
+	"github.com/blackestwhite/zwrapper/service"
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 func Permitted() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		accessTokenService := service.AccessTokenService{}
 		accessToken := c.GetHeader("x-zwrapper-access-token")
 		if len(accessToken) < 36 {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, presenter.Std{
@@ -23,13 +21,8 @@ func Permitted() gin.HandlerFunc {
 			return
 		}
 
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-		defer cancel()
-
-		result := db.Client.Database("zwrapper").Collection("tokens").FindOne(ctx, bson.M{
-			"token": accessToken,
-		})
-		if result.Err() != nil {
+		_, err := accessTokenService.GetByToken(accessToken)
+		if err != nil {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, presenter.Std{
 				Ok:               false,
 				ErrorCode:        http.StatusUnauthorized,
--- service/access.go
+++ service/access.go
@@ -7,6 +7,7 @@ import (
 	"github.com/blackestwhite/zwrapper/db"
 	"github.com/blackestwhite/zwrapper/entity"
 	"github.com/gofrs/uuid"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type AccessTokenService struct{}
@@ -23,4 +24,14 @@ func (a *AccessTokenService) Create(instance entity.AccessToken) (at entity.Acce
 
 	_, err = db.Client.Database("zwrapper").Collection("tokens").InsertOne(ctx, instance)
 	return instance, err
+}
+
+func (a *AccessTokenService) GetByToken(token string) (at entity.AccessToken, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	err = db.Client.Database("zwrapper").Collection("tokens").FindOne(ctx, bson.M{
+		"token": token,
+	}).Decode(&at)
+	return
 }
