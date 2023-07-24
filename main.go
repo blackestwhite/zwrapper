cc83cc9237f85018a1c85ac1172cf4ac726890e6
blackestwhite <pesaregoal@gmail.com>
2023-07-24 07:25:19 +0330

    feat: add graceful shutdown

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/blackestwhite/zwrapper/config"
 	"github.com/blackestwhite/zwrapper/db"
@@ -20,5 +26,36 @@ func main() {
 	engine := gin.New()
 	engine.LoadHTMLGlob("./templates/*")
 	router.Setup(engine)
-	log.Panic(engine.Run(":8080"))
+
+	// Create a channel to receive the OS signals
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// Start the server in a separate goroutine
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: engine,
+	}
+
+	go func() {
+		log.Println("Starting server on :8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
+
+	// Wait for an OS signal to shut down the server gracefully
+	<-quit
+	log.Println("Received OS signal. Shutting down gracefully...")
+
+	// Set a timeout for the graceful shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Shutdown the server with the given context
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Error shutting down server: %v", err)
+	}
+
+	log.Println("Server shut down.")
 }
